0bf028f7e84d315bcc5162371b20c944031bf0e7
itadmin <hadwareontrial>
2025-10-22 10:20:27 +0700

    remove unused import/comment

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,10 @@
-import express, { Express } from "express";
+import express, { Express, Request, Response } from "express";
 import cors from 'cors';
 import morgan from 'morgan';
 import path from 'path';
 import { errorHandler } from './middlewares/error.middleware';
 import { AuthRoutes } from './routes/auth.route';
 import { UserRoutes } from "./routes/user.route";
-// import { MonitoringAbsensiRoutes } from './routes/monitoring-absensi.route';
 import { TimbanganKomposRoutes } from './routes/timbangan-kompos.route';
 
 export class App {
@@ -24,18 +23,16 @@ export class App {
     this.expressApp.use(morgan('dev'));
   }
   private initRoutes() {
-    // const monitoringAbsensiRoutes = new MonitoringAbsensiRoutes();
     const timbanganKomposRoutes = new TimbanganKomposRoutes();
     const authRoutes = new AuthRoutes();
     const userRoutes = new UserRoutes();
 
-    this.expressApp.get('/', (_req, res) => res.send('MRI Backend ExpressJS'));
-    this.expressApp.get('/api/health', (_req, res) => res.send('OK'));
+    this.expressApp.get('/', (_req:Request, res:Response) => res.send('MRI Backend ExpressJS'));
+    this.expressApp.get('/api/health', (_req:Request, res:Response) => res.send('OK'));
 
     this.expressApp.use('/api/auth', authRoutes.router);
     this.expressApp.use('/api/user', userRoutes.router);
     this.expressApp.use('/api/timbangan-kompos', timbanganKomposRoutes.router);
-    // this.expressApp.use('/monitoring-absensi', monitoringAbsensiRoutes.router);
     this.expressApp.use(express.static(path.join(__dirname, "../public")));
   }
 
