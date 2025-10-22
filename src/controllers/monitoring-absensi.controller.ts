a48db80bb61fd80a202cc5dd4150527da46fee6f
itadmin <hadwareontrial>
2025-10-22 10:20:54 +0700

    remove unused files

--- src/controllers/monitoring-absensi.controller.ts
+++
@@ -1,8 +0,0 @@
-import { Request, Response, NextFunction } from 'express';
-// import { MonitoringAbsensiService } from './../services/monitoring-absensi.service';
-
-export class MonitoringAbsensiController {
-  // private service = new MonitoringAbsensiService();
-
-  // controller here
-}
