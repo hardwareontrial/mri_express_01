bb92d7ecb97aed012a297a2611407330e3e772e3
itadmin <hadwareontrial>
2025-10-27 10:25:33 +0700

    change clientId

--- src/services/mqtt-service.ts
+++ src/services/mqtt-service.ts
@@ -22,7 +22,7 @@ export class MqttService {
   async init() {
     try {
       if(!this.client) this.client = mqtt.connect(broker, {
-        clientId: 'mri-server-001-demo',
+        clientId: 'mri-server-001',
         clean: true,
       });
 
